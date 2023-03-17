901325693ab48833acec56d090453badb6554168
RunThem <iccy.fun@outlook.com>
2023-03-18 02:19:59 +0800

    update

--- plugin/fzy.lua
+++
@@ -1,5 +0,0 @@
-if vim.ui then
-  function vim.ui.select(items, opts, on_choice) -- luacheck: ignore 122
-    return require('fzy').pick_one(items, opts.prompt, opts.format_item, on_choice)
-  end
-end
