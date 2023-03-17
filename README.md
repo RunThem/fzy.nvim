3990159ba1e15bf8f15fa3b38d47c0434c6c7e72
RunThem <iccy.fun@outlook.com>
2023-03-18 02:06:44 +0800

    update

--- README.md
+++ README.md
@@ -1,56 +1,27 @@
-# nvim-fzy
+# fzy.nvim
 
-A `vim.ui.select` implementation for the [fzy][2] CLI.
-
+A `vim.ui.select` implementation for the `fzy` CLI.
 
 ## Installation
 
 - Requires Neovim (latest stable or nightly)
-- Install [fzy][2] and make sure it is in your `$PATH`.
-- nvim-fzy is a plugin. Install it like any other plugin:
-  - If using [vim-plug][5]: `Plug 'mfussenegger/nvim-fzy'`
-  - If using [packer.nvim][6]: `use 'mfussenegger/nvim-fzy'`
+- Install `fzy` and make sure it is in your `$PATH`.
 
 ## Usage
 
-`nvim-fzy` provides the `vim.ui.select` implementation and a `execute` function
+`fzy.nvim` provides the `vim.ui.select` implementation and a `execute` function
 to feed the output of commands to `fzy`.
 
 Create some mappings like this:
 
-```vimL
-lua fzy = require('fzy')
-nnoremap <silent><leader>ff :lua fzy.execute('fd', fzy.sinks.edit_file)<CR>
-nnoremap <silent><leader>fg :lua fzy.execute('git ls-files', fzy.sinks.edit_file)<CR>
-nnoremap <silent><leader>fl :lua fzy.execute('ag --nobreak --noheading .', fzy.sinks.edit_live_grep)<CR>
+```lua
+vim.keymap.set('n', '<Leader>ff', function ()
+  local fzy = require('fzy')
+  fzy.execute('fd', fzy.sinks.edit_file)
+end)
+
+vim.keymap.set('n', '<Leader>fa', function ()
+  local fzy = require('fzy')
+  fzy.execute('rg --no-heading --trim -nH .', fzy.sinks.edit_live_grep)
+end)
 ```
-
-See `:help fzy` for more information
-
-![demo](demo/demo.gif)
-
-To get additional pickers to jump to tags and other stuff, you can use it in combination with [nvim-qwahl](https://github.com/mfussenegger/nvim-qwahl)
-
-Enjoy
-
-
-## Goals
-
-- Have a simple API so people can create their custom actions.
-- Include the essentials, but not much more than that.
-
-This plugin is pretty much *done* and won't see feature additions, unless there
-is a convincing case to be made.
-
-
-## Alternatives
-
-- [fzf.vim][1]
-- [telescope.nvim][4]
-
-
-[1]: https://github.com/junegunn/fzf.vim
-[2]: https://github.com/jhawthorn/fzy
-[4]: https://github.com/nvim-lua/telescope.nvim
-[5]: https://github.com/junegunn/vim-plug
-[6]: https://github.com/wbthomason/packer.nvim
--- lua/fzy.lua
+++ lua/fzy.lua
@@ -2,28 +2,26 @@ local api = vim.api
 local vfn = vim.fn
 local M = {}
 
-
 function M.new_popup()
   local buf = api.nvim_create_buf(false, true)
   api.nvim_buf_set_keymap(buf, 't', '<ESC>', '<C-\\><C-c>', {})
   api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  local columns = api.nvim_get_option('columns')
+
   local lines = api.nvim_get_option('lines')
-  local width = math.floor(columns * 0.9)
-  local height = math.floor(lines * 0.8)
+
   local opts = {
     relative = 'editor',
     style = 'minimal',
-    row = math.floor((lines - height) * 0.5),
-    col = math.floor((columns - width) * 0.5),
-    width = width,
-    height = height,
+    row = lines - 24,
+    col = 5,
+    width = 120,
+    height = 18,
     border = 'single',
   }
+
   local win = api.nvim_open_win(buf, true, opts)
   return win, buf
 end
-
 
 local sinks = {}
 M.sinks = sinks
@@ -33,66 +31,18 @@ function sinks.edit_file(selection)
   end
 end
 
-
 function sinks.edit_live_grep(selection)
   -- fzy returns search input if zero results found. This case is mapped to nil as well.
-  selection = string.match(selection, ".+:%d+:.+")
+  selection = string.match(selection, '.+:%d+: .+')
   if selection then
-    local parts = vim.split(selection, ":")
+    local parts = vim.split(selection, ':')
     local path, line = parts[1], parts[2]
-    vim.cmd("e +" .. line .. " " .. path)
+    vim.cmd('e +' .. line .. ' ' .. path)
   end
 end
 
-
-function M.pick_one(items, prompt, label_fn, cb)
-  label_fn = label_fn or vim.inspect
-  local num_digits = math.floor(math.log(math.abs(#items), 10) + 1)
-  local digit_fmt = '%0' .. tostring(num_digits) .. 'd'
-  local inputs = vfn.tempname()
-  vfn.system(string.format('mkfifo "%s"', inputs))
-  local co
-  if cb == nil then
-    co = coroutine.running()
-    assert(co, "If callback is nil the function must run in a coroutine")
-    cb = function(choice, idx)
-      coroutine.resume(co, choice, idx)
-    end
-  end
-
-  M.execute(
-    string.format('cat "%s"', inputs),
-    function(selection)
-      os.remove(inputs)
-      if not selection or vim.trim(selection) == '' then
-        cb(nil)
-      else
-        local parts = vim.split(selection, '│ ')
-        local idx = tonumber(parts[1])
-        cb(items[idx], idx)
-      end
-    end,
-    prompt
-  )
-  local f = io.open(inputs, 'a')
-  if f then
-    for i, item in ipairs(items) do
-      local label = string.format(digit_fmt .. '│ %s', i, label_fn(item))
-      f:write(label .. '\n')
-    end
-    f:flush()
-    f:close()
-  else
-    vim.notify('Could not open tempfile', vim.log.levels.ERROR)
-  end
-  if co then
-    return coroutine.yield()
-  end
-end
-
-
-function M.execute(choices_cmd, on_selection, prompt)
-  if api.nvim_get_mode().mode == "i" then
+function M.execute(choices_cmd, on_selection)
+  if api.nvim_get_mode().mode == 'i' then
     vim.cmd('stopinsert')
   end
   local tmpfile = vfn.tempname()
@@ -101,28 +51,16 @@ function M.execute(choices_cmd, on_selection, prompt)
   local popup_win, buf = M.new_popup()
   local height = api.nvim_win_get_height(popup_win)
   local fzy
-  if prompt then
-    fzy = string.format(
-      '%s | fzy -l %d -p %s > "%s"',
-      choices_cmd,
-      height,
-      vim.fn.shellescape(prompt),
-      tmpfile
-    )
-  else
-    fzy = string.format(
-      '%s | fzy -l %d > "%s"',
-      choices_cmd,
-      height,
-      tmpfile
-    )
-  end
-  api.nvim_create_autocmd({'TermOpen', 'BufEnter'}, {
+
+  fzy = string.format('%s | fzy -l %d -p "_> " > "%s"', choices_cmd, height, tmpfile)
+  api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
     buffer = buf,
     command = 'startinsert!',
     once = true,
   })
-  local args = {shell, shellcmdflag, fzy}
+
+  local args = { shell, shellcmdflag, fzy }
+
   vfn.termopen(args, {
     on_exit = function()
       -- popup could already be gone if user closes it manually; Ignore that case
@@ -130,7 +68,7 @@ function M.execute(choices_cmd, on_selection, prompt)
       local contents = nil
       local file = io.open(tmpfile)
       if file then
-        contents = file:read("*all")
+        contents = file:read('*all')
         file:close()
         os.remove(tmpfile)
       end
@@ -150,9 +88,8 @@ function M.execute(choices_cmd, on_selection, prompt)
       else
         on_selection(contents)
       end
-    end;
+    end,
   })
 end
-
 
 return M
